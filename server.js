bfb77c6f664e16398316cc3537295046352e494d
Anastasi Bakolias <s.bakolias@gmail.com>
2014-12-14 12:48:39 -0800

    used server.js to tie app together, intitialize express app, require the routes (passing in the express app) and put up server

---
+++ server.js
@@ -0,0 +1,12 @@
+var pg = require('pg'),
+    ejs = require('ejs'),
+    express = require('express');
+
+var app = express();
+app.use('view engine', 'ejs');
+
+
+require(./app/routes.js)(app);
+
+app.listen(port);
+console.log('embark at port ' + port);
