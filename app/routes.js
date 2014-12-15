dbdfc4508297e1ed243b8bfd04f6df7fbeb0097f
Tessa <tessakkelly@gmail.com>
2014-12-14 18:15:09 -0800

    add requests page to show outstanding borrowed items and requests and items which user has borrowed

--- app/routes.js
+++ app/routes.js
@@ -4,8 +4,14 @@ var items = require('./repos/Items.js')(pg, conString);
 module.exports = function(app){
 
   var userItems = [{name: "Elephant", description: "test description", image_link: "http://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/330px-African_Bush_Elephant.jpg"},
-                    {name: "MacDougal", description: "I'm another elephant. Hi!", image_link: "http://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Elephas_maximus_%28Bandipur%29.jpg/1280px-Elephas_maximus_%28Bandipur%29.jpg"}]
+                    {name: "MacDougal", description: "I'm another elephant. Hi!", image_link: "http://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Elephas_maximus_%28Bandipur%29.jpg/1280px-Elephas_maximus_%28Bandipur%29.jpg"}];
   // userItems = items.getAllForUserId();
+
+  var userRequests = [{borrower: "Elphie", item: "hairdryer"},
+                      {borrower: "McSnazzie", item: "bedazzled pants"}];
+
+  var borrowed = [{name: "chainsaw", owner: "Gilligan"},
+                  {name: "speakers", owner: "Mikintosh"}]
 
   app.get('/', function(request, response){
     response.render('list_items.ejs', {userItems: userItems, user: "Maximus the Parakeet"});
@@ -14,5 +20,9 @@ module.exports = function(app){
   app.get('/items', function(request, response){
     response.render('thumb_items.ejs', {userItems: userItems, user: "Maximus the Parakeet"});
   });
+
+  app.get('/requests', function(request, response){
+    response.render('requests.ejs', {userRequests: userRequests, borrowed: borrowed})
+  })
 
 };
