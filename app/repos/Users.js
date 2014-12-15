f741843f93fce0d0866afd9aa306d6bf15f597f0
Sean Gibat <sgibat@gmail.com>
2014-12-15 09:37:39 -0800

    Refactored repo code. moved all query logic into one function that a querystring and callback are passed into in all repos.

--- app/repos/Users.js
+++ app/repos/Users.js
@@ -1,139 +1,89 @@
 
 // var pg = require('pg'), conString = "postgres://something";
 module.exports = function(pg,conString){
-  var createTable = function(callback){
+
+  var dbQuery = function(callback, queryString, array){
     pg.connect(conString, function(err, client, done){
       if(err) return console.log('error fetching client from pool', err);
-      var queryString = "\
-        CREATE TABLE IF NOT EXISTS users ( \
-          id SERIAL PRIMARY KEY \
-          name TEXT, \
-          email TEXT, \
-          password TEXT \
-        );\
-      ";
-
-      client.query(queryString, function(err, result){
+      client.query(queryString, array, function(err, result){
         done();
-        callback(result);
         if(err) return console.log('error running query', err);
+        result.rows = (result.rows.length > 0) ? result.rows : null;
+        if (callback) callback(result.rows);
+        return;
       });
     });
   };
 
-  var dropTable = function(callback){
-    pg.connect(conString, function(err, client, done){
-      if(err) return console.log('error fetching client from pool', err);
-      var queryString = "DROP TABLE users;"
+  var createTableFn = function(callback){
+    var queryString = "\
+      CREATE TABLE IF NOT EXISTS users ( \
+        id SERIAL PRIMARY KEY, \
+        name TEXT, \
+        email TEXT, \
+        password TEXT \
+      );\
+    ";
+    dbQuery(callback, queryString, [1]);
+  };
 
-      client.query(queryString, function(err, result){
-        done();
-        callback(result);
-        if(err) return console.log('error running query', err);
-      });
-    });
+  var dropTableFn = function(callback){
+    var queryString = "DROP TABLE users;"
+    dbQuery(callback, queryString, [1]);
   };
 
   var getAllFn = function(callback){
-    pg.connect(conString, function(err, client, done){
-      if(err) return console.log('error fetching client from pool', err);
-      var queryString = " \
-        SELECT name, email, id \
-        FROM users; \
-      ";
-
-      client.query(queryString, function(err, result){
-        done();
-        if(err) return console.log('error running query', err);
-        result.rows = (result.rows.length > 0) ? result.rows : null;
-        callback(result.rows);
-        return;
-      });
-    }); 
+    var queryString = " \
+      SELECT name, email, id \
+      FROM users; \
+    ";
+    dbQuery(callback, queryString, [1]);
   };
 
-  var newUserFn = function(params, callback){
-    pg.connect(conString, function(err, client, done){
-      if(err) return console.log('error fetching client from pool', err);
-      var queryString = " \
-        INSERT INTO users (name, email, password) \
-        VALUES ($1,$2,$3) \
-        RETURNING id; \
-      ";
-
-      client.query(queryString, [params.name, params.email, params.password], function(err, result){
-        done();
-        if(err) return console.log('error running query', err);
-        result.rows = (result.rows.length > 0) ? result.rows : null;
-        callback(result.rows);
-        return;
-      });
-    }); 
-  };
+  var newUserFn =  function(params, callback){
+    var queryString = " \
+      INSERT INTO users (name, email, password) \
+      VALUES ($1,$2,$3) \
+      RETURNING id; \
+    ";
+    dbQuery(callback, queryString, [params.name, params.email, params.password]);
+  }
 
   var deleteUserFn = function(params, callback){
-    pg.connect(conString, function(err, client, done){
-      if(err) return console.log('error fetching client from pool', err);
-      var queryString = "\
-        DELETE FROM users \
-        WHERE id=$1; \
-      ";
-
-      client.query(queryString, [params.id], function(err, result){
-        done();
-        if(err) return console.log('error running query', err);
-        result.rows = (result.rows.length > 0) ? result.rows : null;
-        callback(result.rows);
-        return;
-      });
-    }); 
+    var queryString = "\
+      DELETE FROM users \
+      WHERE id=$1; \
+    ";
+    dbQuery(callback, queryString, [params.id]);
   };
 
   var getByIdFn = function(params, callback){
-    pg.connect(conString, function(err, client, done){
-      if(err) return console.log('error fetching client from pool', err);
-      var queryString = "\
-        SELECT * \
-        FROM users \
-        WHERE id=$1; \
-      ";
-
-      client.query(queryString, [params.id], function(err, result){
-        done();
-        if(err) return console.log('error running query', err);
-        result.rows = (result.rows.length > 0) ? result.rows : null;
-        callback(result.rows);
-        return;
-      });
-    }); 
-  };
+    var queryString = "\
+      SELECT * \
+      FROM users \
+      WHERE id=$1; \
+    ";
+    dbQuery(callback, queryString, [params.id]);
+  }
 
   var getByEmailFn = function(params, callback){
-    pg.connect(conString, function(err, client, done){
-      if(err) return console.log('error fetching client from pool', err);
-      var queryString = "\
-        SELECT * \
-        FROM users \
-        WHERE email=$1; \
-      ";
-
-      client.query(queryString, [params.email], function(err, result){
-        done();
-        if(err) return console.log('error running query', err);
-        result.rows = (result.rows.length > 0) ? result.rows : null;
-        callback(result.rows);
-        return;
-      });
-    }); 
+    var queryString = "\
+      SELECT * \
+      FROM users \
+      WHERE email=$1; \
+    ";
+    dbQuery(callback, queryString, [params.email]);
   };
 
-  createTable();
+  createTableFn();
 
   return {
-    getById    : getByIdFn,
-    getByEmail : getByEmailFn,
-    deleteUser : deleteUserFn,
-    newUser    : newUserFn,
-    getAll     : getAllFn
+    getById     : getByIdFn,
+    getByEmail  : getByEmailFn,
+    deleteUser  : deleteUserFn,
+    newUser     : newUserFn,
+    getAll      : getAllFn,
+    createTable : createTableFn,
+    dropTable   : dropTableFn
   }
 };
