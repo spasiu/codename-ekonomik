8edb8bc527ecf143cf4d9b7495bcd78219c5669b
Tessa <tessakkelly@gmail.com>
2014-12-15 10:40:59 -0800

    add passport, add passport-facebook, add to auth.js

--- app/auth.js
+++ app/auth.js
@@ -1,7 +1,31 @@
 module.exports = function(pg, conString){
-  var auth = function(email, password, callback){
-    // if()
-  };
-  return auth;
-};
+//   var auth = function(email, password, callback){
+//     // if()
+//   };
+//   return auth;
 
+  var passport = require('passport'), FacebookStrategy = require('passport-facebook').Strategy;
+
+  passport.use(new FacebookStrategy({
+      clientID: 1511022942514049,
+      clientSecret: fa6df13e2141d4fd138c1eb769d005a0,
+      callbackURL: "http://localhost:3000/auth/facebook/callback" // Note: For security reasons, the redirection URL must reside on the same host that is registered with Facebook.
+    },
+    function(accessToken, refreshToken, profile, done) {
+      users.findOrCreate({ 'facebook.id': profile.id }, 
+        function(err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          user = new User({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            facbook: profile._json
+          });
+          users.newUser(user);
+        } else {
+          return done(err, user);
+        }
+      });
+    }
+  ));
+};
--- app/routes.js
+++ app/routes.js
@@ -37,4 +37,10 @@ module.exports = function(app){
       // bodyParser middleware?
     });
 
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/callback', 
+    passport.authenticate('facebook', { successRedirect: '/', 
+                                        failureRedirect: '/login' }));
+
 };
